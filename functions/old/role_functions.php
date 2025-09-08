8778f6d6812bca13ac133b37e55b1774f9dd5394
Braun Okoi Boniface <okoibraun@gmail.com>
2025-09-08 16:18:35 +0100

    Cleaned up the project and removed unwanted and unused files and directories for live/production

--- functions/old/role_functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function assign_role_to_user($user_id, $company_id, $role_id) {
-    global $conn;
-    $stmt = $conn->prepare("INSERT INTO user_company_roles (user_id, company_id, role_id) VALUES (?, ?, ?)");
-    $stmt->bind_param("iii", $user_id, $company_id, $role_id);
-    return $stmt->execute();
-}
